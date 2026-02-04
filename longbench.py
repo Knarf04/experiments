f59d58322dc8e64b4a2e3dd8b154ddce4d5f0948
Haochen Shen <shenhaochen2000@gmail.com>
2026-02-03 19:47:12 -0600

    update

--- longbench.py
+++
@@ -1,7 +0,0 @@
-from datasets import load_dataset
-
-data_dir = "/gpfs/hshen/datasets/longbench-v1"
-data_dir = None
-ds = load_dataset("longbench_local.py", name="qasper", data_dir=data_dir, split="test")
-first_two = ds[:2]
-print(first_two)
---
+++ model_sanity_check.py
@@ -0,0 +1,23 @@
+import torch
+from transformers import AutoTokenizer, AutoModelForCausalLM
+
+
+model_path = "/gpfs/goon/models/granite-4-lite/run-20250607-phase3-annealing-with-fim-revised-mix/hf"
+model = AutoModelForCausalLM(model_path, torch_dtype=torch.bfloat16, low_cpu_mem_usage=True)
+
+tokenizer = AutoTokenizer(model_path)
+model.eval()
+
+prompt = "The capital city of USA is"
+inputs = tokenizer(prompt, return_tensors="pt")
+
+with torch.no_grad():
+    out = model.generate(
+        **inputs,
+        max_new_tokens=10,
+        do_sample=False,   # greedy
+        eos_token_id=tokenizer.eos_token_id,
+        pad_token_id=tokenizer.eos_token_id,
+    )
+
+print(tokenizer.decode(out[0], skip_special_tokens=True))
